280871cd211d23f0fcf41a67a0196186f1d2f21f
yurim0628 <139152515+yurim0628@users.noreply.github.com>
2024-03-15 00:45:13 +0900

    [level 2] Title: 택배상자, Time: 299.27 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -1,14 +1,12 @@
 def solution(order):
     answer = 0
     stack = []
-    box_number = 1
-    i = 0
+    index = 0
     
-    while box_number != len(order) + 1:
+    for box_number in range(1, len(order) + 1):
         stack.append(box_number)
-        while stack and stack[-1] == order[i]:
+        while stack and stack[-1] == order[index]:
             stack.pop()
-            i += 1
-            answer += 1
-        box_number += 1
+            index, answer = index + 1, answer + 1 
+            
     return answer
