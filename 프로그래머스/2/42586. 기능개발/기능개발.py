c46c11411519358727203ee457291d740305748d
yurim0628 <139152515+yurim0628@users.noreply.github.com>
2024-03-20 04:57:31 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,17 @@
+def solution(progresses, speeds):
+    answer = []
+    count = 0  
+    while progresses:
+        for i in range(len(progresses)):
+            progresses[i] += speeds[i]  
+
+        if progresses[0] >= 100:  
+            while progresses and progresses[0] >= 100:
+                progresses.pop(0)  
+                speeds.pop(0)  
+                count += 1
+
+            answer.append(count)  
+            count = 0
+
+    return answer
