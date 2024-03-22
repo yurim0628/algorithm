33ea59c9094fcc5b7447f325a102ef2c2a1f4a97
yurim0628 <139152515+yurim0628@users.noreply.github.com>
2024-03-22 18:30:39 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.51 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -5,28 +5,36 @@ def solution(n, wires):
         count = 0
         
         queue = deque()
-        visited = [False] * (n+1)
+        visited = [0] * (n+1)
         
         queue.append(s)
-        visited[s] = True
+        visited[s] = 1
         
         while queue:
             cur = queue.popleft()
-            for nxt in arr[cur]:
-                if not visited[nxt]:
-                    visited[nxt] = True
-                    queue.append(nxt)
-                    count += 1
+            
+            if arr[cur]:
+                for nxt in arr[cur]:
+                    if not visited[nxt]:
+                        visited[nxt] = 1
+                        queue.append(nxt)
+                        count += 1
+                        
         return count
         
+    arr = [[] for _ in range(n+1)]
+    for a, b in wires:
+        arr[a].append(b)
+        arr[b].append(a)
+        
     answer = float('inf')  
-    for i, _ in enumerate(wires): 
-        arr = [[] for _ in range(n+1)] 
-        for j, wire in enumerate(wires): 
-            if j != i: 
-                arr[wire[0]].append(wire[1])
-                arr[wire[1]].append(wire[0])
-        min_diff = abs(bfs(wires[i][0]) - bfs(wires[i][1]))  
-        answer = min(answer, min_diff)  
-          
+    for a, b in wires:
+        arr[a].remove(b)
+        arr[b].remove(a)
+        
+        answer = min(answer, abs(bfs(a) - bfs(b)))
+        
+        arr[a].append(b)
+        arr[b].append(a)
+        
     return answer
