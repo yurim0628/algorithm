4e5d82a32a17ad366758d3049ede3bac8bbc0fbf
yurim0628 <139152515+yurim0628@users.noreply.github.com>
2024-04-23 18:07:13 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 233.94 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,32 +1,31 @@
-from collections import defaultdict
-
 def solution(gems):
     answer = [0, len(gems)]
-    num_unique_gems = len(set(gems))
+    
+    gems_set = set(gems)
+    gems_length = len(gems_set)
 
-    if num_unique_gems == 1:
+    if gems_length == 1:
         return [1, 1]
-
-    gem_indices = defaultdict(list)
-    start_idx, end_idx = 0, 0
-    gem_count = 0
-
-    while end_idx < len(gems):
-        current_gem = gems[end_idx]
-        if not gem_indices[current_gem]:
-            gem_count += 1
-        gem_indices[current_gem].append(end_idx)
-
-        while gem_count == num_unique_gems:
-            if end_idx - start_idx < answer[1] - answer[0]:
-                answer = [start_idx, end_idx]
-
-            start_gem = gems[start_idx]
-            gem_indices[start_gem].pop(0)
-            if not gem_indices[start_gem]:
-                gem_count -= 1
-            start_idx += 1
-
-        end_idx += 1
+    
+    start, end, gems_count = 0, 1, 1
+    gems_dict = {gems[start]: [0]}
+    while end < len(gems):
+        gem = gems[end]
+        if gem in gems_dict:
+            gems_dict[gem].pop(0)
+            gems_dict[gem].append(end)
+        else:
+            gems_dict[gem] = [end]
+            gems_count += 1
+            
+        if gems_count == gems_length:
+            start = sorted(gems_dict.items(), key=lambda x:x[1])[0][1][0]
+            
+            if end - start < answer[1] - answer[0]:
+                answer = [start, end]
+            start, end, gems_count = start + 1, start + 1, 1
+            gems_dict = {gems[start]: [start]}
+            
+        end += 1
 
     return [answer[0] + 1, answer[1] + 1]
