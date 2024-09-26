1348894413c03cb15e600d59283fbfdd54dc0d5d
yurim0628 <139152515+yurim0628@users.noreply.github.com>
2024-09-26 18:06:47 +0900

    [Silver IV] Title: 큐, Time: 68 ms, Memory: 34036 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline().strip())
+
+queue = deque()
+for _ in range(N):
+    command = sys.stdin.readline().strip().split()
+    
+    if command[0] == 'push':
+        queue.append(int(command[1]))  
+    elif command[0] == 'pop':
+        if queue:
+            print(queue.popleft())  
+        else:
+            print(-1)
+    elif command[0] == 'size':
+        print(len(queue))
+    elif command[0] == 'empty':
+        print(1 if not queue else 0)
+    elif command[0] == 'front':
+        if queue:
+            print(queue[0]) 
+        else:
+            print(-1)
+    elif command[0] == 'back':
+        if queue:
+            print(queue[-1]) 
+        else:
+            print(-1)
