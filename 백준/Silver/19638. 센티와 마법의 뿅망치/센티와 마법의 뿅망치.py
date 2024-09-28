d674f662299f09253cb3bd20330e0e1fd97d50bf
yurim0628 <139152515+yurim0628@users.noreply.github.com>
2024-09-28 13:17:22 +0900

    [Silver I] Title: 센티와 마법의 뿅망치, Time: 140 ms, Memory: 37044 KB -BaekjoonHub

--- 백준/Silver/19638. 센티와 마법의 뿅망치/센티와 마법의 뿅망치.py
+++ 백준/Silver/19638. 센티와 마법의 뿅망치/센티와 마법의 뿅망치.py
@@ -4,31 +4,32 @@ import heapq
 input = sys.stdin.readline
 
 n, h, t = map(int, input().split())
+
 heights = []
 count = 0
 
-# 거인들의 키 입력받기
 for _ in range(n):
-    heapq.heappush(heights, -int(input()))  # 음수로 저장하여 최소 힙 사용
+    heapq.heappush(heights, -int(input())) 
 
-for j in range(t):
-    max_height = -heapq.heappop(heights)  # 최대 키를 가져오기
+for _ in range(t):
+    max_height = -heapq.heappop(heights) 
     if max_height < h:
         print("YES")
-        print(count)  # 사용한 횟수
+        print(count)  
         break
-    elif max_height == 1:
-        print("NO")
-        print(1)  # 남은 횟수
-        break
-    else:
-        heapq.heappush(heights, -(max_height // 2))  # 키를 절반으로 줄이기
+    if max_height > 1:
+        heapq.heappush(heights, -(max_height // 2))  
         count += 1
+    else:
+        print("NO")
+        print(1)
+        break
+     
 else:
-    max_height = -heapq.heappop(heights)  # 가장 큰 거인의 키 가져오기
+    max_height = -heapq.heappop(heights) 
     if max_height < h:
         print("YES")
         print(count)
     else:
         print("NO")
-        print(max_height)  # 가장 큰 거인의 키 출력
+        print(max_height) 
