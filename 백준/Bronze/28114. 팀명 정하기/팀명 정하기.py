e89aa813aca2cafae76d1f009ce6667ea817b7aa
yurim0628 <139152515+yurim0628@users.noreply.github.com>
2024-10-03 14:19:54 +0900

    [Bronze II] Title: 팀명 정하기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/28114. 팀명 정하기/팀명 정하기.py
+++ 백준/Bronze/28114. 팀명 정하기/팀명 정하기.py
@@ -5,9 +5,6 @@ for _ in range(3):
     score, year, last_name = input().split()
     score_dict[last_name] = int(score)
     years.append(year[2:])  
-
-years.sort()
-print(''.join(year for year in years))
-
-sorted_scores = sorted(score_dict.items(), key=lambda item: item[1], reverse=True)
-print(''.join(name[0] for name, score in sorted_scores))
+    
+print(''.join(year for year in sorted(years)))
+print(''.join(name[0] for name, score in sorted(score_dict.items(), key=lambda item: item[1], reverse=True)))
