b2cd585707a052f647459961bcbbc0feac7719e7
yurim0628 <139152515+yurim0628@users.noreply.github.com>
2025-04-01 16:45:26 +0900

    [level 2] Title: 점 찍기, Time: 16.33 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public long solution(long k, long d) {
+        long answer = 0;     
+        
+        for (long x = 0; x <= d; x += k) {
+            long y = (long) Math.sqrt(d*d - x*x);
+            answer += y / k + 1;
+        }
+        
+        return answer;
+    }
+}
