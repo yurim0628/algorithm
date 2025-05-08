56ba7a48592f2df973f920f5f61f188a9fbf95b8
yurim0628 <139152515+yurim0628@users.noreply.github.com>
2025-05-09 03:55:03 +0900

    [level 0] Title: 약수 구하기, Time: 0.15 ms, Memory: 94.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> divisors = new ArrayList<>();
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                divisors.add(i);
+                if (i * i != n) {
+                    divisors.add(n / i);
+                }
+            }
+        }
+        Collections.sort(divisors);
+        int[] result = new int[divisors.size()];
+        for (int i = 0; i < divisors.size(); i++) {
+            result[i] = divisors.get(i);
+        }
+        return result;
+    }
+}
