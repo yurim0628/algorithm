189175b16352a5a04708e0bcf42c43960cd86ea8
yurim0628 <139152515+yurim0628@users.noreply.github.com>
2025-07-23 21:40:21 +0900

    [level 3] Title: 거스름돈, Time: 13.14 ms, Memory: 54.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n, int[] money) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1; 
+
+        for (int coin : money) {
+            for (int i = coin; i <= n; i++) {
+                dp[i] = (dp[i] + dp[i - coin]) % 1000000007;
+            }
+        }
+
+        return dp[n];
+    }
+}
